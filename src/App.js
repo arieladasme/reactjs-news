3eb9e97c9cdfde8c7e05079781ac7b0c692cb3bd
Ariel <ariel.wm1@gmail.com>
2020-07-18 17:09:41 -0400

    quering newsapi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import Header from './components/Header'
 import Form from './components/Form'
 
 function App() {
   // Category and news
   const [category, saveCategory] = useState('')
+  const [news, saveNews] = useState([])
+
+  useEffect(() => {
+    const queryAPI = async () => {
+      const apiKey = 'c1543686c454466fa5bb1899502677f4'
+      const url = `http://newsapi.org/v2/top-headlines?country=ar&category=${category}&apiKey=${apiKey}`
+
+      const response = await fetch(url)
+      const news = await response.json()
+      saveNews(news.articles)
+    }
+    queryAPI()
+  }, [category])
 
   return (
     <Fragment>
